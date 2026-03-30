1cd1009dc166e5dd06d23df0f2e218bd3ecb325a
Dadu400 <daduka.xuxunashvili@gmail.com>
2026-03-30 20:55:30 +0400

    implement header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/app/components/header/Header";
+import Footer from "@/app/components/footer/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <>
+          <Header />
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer />
+        </>
+      </body>
     </html>
   );
 }
