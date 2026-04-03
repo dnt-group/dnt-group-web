451453971c40ad4ec3d8021dbf25e54a8198b7ef
Dadu400 <daduka.xuxunashvili@gmail.com>
2026-04-03 15:47:27 +0400

    remove language params from url

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "ka"],
   defaultLocale: "en",
+  localePrefix: "never",
 });
 
 export type Locale = (typeof routing.locales)[number];
